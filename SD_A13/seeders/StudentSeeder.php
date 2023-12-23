075741a8f9512a01acd22c67f256c40419ed03af
Dhrubajyoti Das <dhrubajyoti1212@gmail.com>
2023-12-23 17:00:16 +0600

    Add files via upload

--- SD_A13/seeders/StudentSeeder.php
+++ SD_A13/seeders/StudentSeeder.php
@@ -13,11 +13,36 @@ class StudentSeeder extends Seeder
      */
     public function run()
     {
-        student::create([
-            'name'=>'abcd',
-            'email'=>'abcd@gmail.com',
-            'city'=>'Dhaka',
-            'contactNo'=>1234545
+        $students = collect([
+            [
+                'name'=>'abcd',
+                'email'=>'abcd@gmail.com',
+                'city'=>'Dhaka',
+                'contactNo'=>125657
+            ],
+            [
+                'name'=>'cbd',
+                'email'=>'cbd@gmail.com',
+                'city'=>'Ctg',
+                'contactNo'=>765365
+            ],
+            [
+                'name'=>'eiort',
+                'email'=>'eiort@gmail.com',
+                'city'=>'Ctg',
+                'contactNo'=>14636
+            ]
         ]);
+
+        $students->each(function ($student) {
+            student::insert($student);
+        });
+
+        // student::create([
+        //     'name'=>'abcd',
+        //     'email'=>'abcd@gmail.com',
+        //     'city'=>'Dhaka',
+        //     'contactNo'=>1234545
+        // ]);
     }
 }
